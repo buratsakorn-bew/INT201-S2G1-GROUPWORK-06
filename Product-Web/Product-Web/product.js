e5d8da960440a43ba7fb82074243f9d53b9099e3
_BBURATS_ <72536741+buratsakorn-bew@users.noreply.github.com>
2021-10-29 17:53:26 +0700

    [add] HW06

---
+++ Product-Web/Product-Web/product.js
@@ -0,0 +1,43 @@
+export let products = [{
+        img: "https://media.discordapp.net/attachments/903253994997157971/903313046787731596/airpods-3rd-generationWHITE.png?width=523&height=669",
+        id: 1001,
+        name: "AirPods (3rd generation)",
+        price: "6,700.00",
+        stock: "0",
+    },
+    {
+        img: "https://media.discordapp.net/attachments/903253994997157971/903316693261115442/airpods-max-select-spacegray-202011.png?width=566&height=670",
+        id: 2001,
+        name: "AirPods Max",
+        price: "19,900.00",
+        stock: "1"
+    },
+    {
+        img: "https://media.discordapp.net/attachments/903253994997157971/903316904842764308/mag-safe-charger.png?width=378&height=669",
+        id: 3001,
+        name: "MagSafe Charger",
+        price: "1,490.00",
+        stock: "5"
+    },
+    {
+        img: "https://media.discordapp.net/attachments/814156252774137899/903587957804511262/MC552.png?width=1056&height=1056",
+        id: 4001,
+        name: "Apple 30-pin to VGA Adapter",
+        price: "1,190.00",
+        stock: "6"
+    },
+    {
+        img: "https://media.discordapp.net/attachments/903253994997157971/903318341358354492/unknown.png?width=894&height=670",
+        id: 5001,
+        name: "Magic Keyboard - Thaik",
+        price: "3,190.00",
+        stock: "9"
+    },
+    {
+        img: "https://media.discordapp.net/attachments/814156252774137899/903587953371148318/applewatch.png",
+        id: 6001,
+        name: "Apple Watch Magnetic Charging Dock",
+        price: "2,990.00",
+        stock: "10"
+    }
+]
